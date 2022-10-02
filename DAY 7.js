eb3d56b0b4858fb975254d2d24cf9a8ac7837722
JAMEEL AHMED <112924673+AJwiz@users.noreply.github.com>
2022-10-02 14:10:42 +0530

    D7t

--- DAY 7.js
+++ DAY 7.js
@@ -10,10 +10,10 @@ request.onload = function () {
     var mrft = JSON.parse(request.response);
     //console.log(mrft);
 // Get all the countries from the Asia continent /region using the Filter function
-    var cr = mrft.filter((ele)=>ele.region =="Asia").map((ele)=>ele.name);
+    var cr = mrft.filter((ele)=>ele.region =="Asia").map((ele)=>ele.name.common);
     console.log(cr);
 // Get all the countries with a population of less than 2 lakhs using Filter function
-    var popu = mrft.filter((ele)=>ele.population < 200000).map((ele)=>ele.name);
+    var popu = mrft.filter((ele)=>ele.population < 200000).map((ele)=>ele.name.common);
     console.log(popu);
 // Print the following details name, capital, flag using forEach function
    mrft.forEach((ele)=>console.log (`Name:${ele.name.common},Capital:${ele.capital},Flag:${ele.flag})`));
